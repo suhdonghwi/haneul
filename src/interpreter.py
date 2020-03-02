46c62ac4cd5fa5c380d3675e989ee8d11e4adfca
suhdonghwi <hwidongsuh@gmail.com>
2020-03-02 21:23:59 +0900

    test.h 로 (임시) 변경

--- src/interpreter.py
+++ src/interpreter.py
@@ -9,7 +9,7 @@ import copy
 
 
 jitdriver = jit.JitDriver(greens=['pc', 'code_object'],
-                          reds=['stack', 'frame', 'self'],
+                          reds=['frame', 'self'],
                           virtualizables=['frame']
                           )
 
@@ -56,14 +56,27 @@ class Env:
 
 
 class Frame:
-  _immutable_fields_ = ['locals']
-  _virtualizable_ = ['locals[*]']
+  _immutable_fields_ = ['locals, stack']
+  _virtualizable_ = ['locals[*], stack[*]']
 
-  def __init__(self, local_number, local_list):
+  def __init__(self, local_number, local_list, max_stack_depth=8):
     self = jit.hint(self, access_directly=True, fresh_virtualizable=True)
 
     resize_list(local_list, local_number)
     self.local_list = local_list
+
+    self.stack = [None] * max_stack_depth
+    self.stack_top = 0
+
+  def push(self, value):
+    self.stack[self.stack_top] = value
+    self.stack_top += 1
+
+  def pop(self):
+    value = self.stack[self.stack_top - 1]
+    self.stack[self.stack_top - 1] = None
+    self.stack_top -= 1
+    return value
 
   @jit.elidable
   def load(self, index):
@@ -101,55 +114,54 @@ class Interpreter:
 
   def run(self, code_object, args):
     pc = 0
-    stack = []
     frame = Frame(code_object.local_number, args)
     code_object = jit.promote(code_object)
 
     while pc < len(code_object.code):
       jitdriver.jit_merge_point(
           pc=pc, code_object=code_object,
-          stack=stack, frame=frame, self=self)
+          frame=frame, self=self)
 
       inst = jit.promote(code_object.code[pc])
       op = jit.promote(inst.opcode)
       try:
         if op == INST_PUSH:
-          stack.append(code_object.get_constant(inst.operand_int))
+          frame.push(code_object.get_constant(inst.operand_int))
 
         elif op == INST_POP:
-          stack.pop()
+          frame.pop()
 
         elif op == INST_LOAD:
-          stack.append(frame.load(inst.operand_int))
+          frame.push(frame.load(inst.operand_int))
 
         elif op == INST_STORE:
-          # frame.append(stack.pop())
-          frame.store(stack.pop(), inst.operand_int)
+          # frame.append(frame.pop())
+          frame.store(frame.pop(), inst.operand_int)
 
         elif op == INST_LOAD_DEREF:
-          stack.append(code_object.free_vars[inst.operand_int])
+          frame.push(code_object.free_vars[inst.operand_int])
 
         elif op == INST_LOAD_GLOBAL:
-          stack.append(self.env.lookup(inst.operand_int))
+          frame.push(self.env.lookup(inst.operand_int))
 
         elif op == INST_STORE_GLOBAL:
-          self.env.store(stack.pop(), inst.operand_int)
+          self.env.store(frame.pop(), inst.operand_int)
 
         elif op == INST_CALL:
           given_arity = len(inst.operand_str)
 
-          value = stack.pop()
+          value = frame.pop()
           josa_map = list(value.josa_map)
           if value.type == TYPE_FUNC:
             args = []
             rest_arity = 0
 
             if len(josa_map) == 1 and josa_map[0][0] == inst.operand_str[0]:
-              args.append(stack.pop())
+              args.append(frame.pop())
             else:
               for josa in inst.operand_str:
                 (j, index) = resolve_josa(josa, josa_map)
-                josa_map[index] = (j, stack.pop())
+                josa_map[index] = (j, frame.pop())
 
               for (_, v) in josa_map:
                 args.append(v)
@@ -159,14 +171,14 @@ class Interpreter:
             if rest_arity > 0:
               func = ConstFunc([], value.funcval, value.builtinval)
               func.josa_map = josa_map
-              stack.append(func)
+              frame.push(func)
             else:  # rest_arity == 0
               if value.builtinval is None:
                 result = self.run(value.funcval, args)
-                stack.append(result)
+                frame.push(result)
               else:
                 func_result = value.builtinval(args)
-                stack.append(func_result)
+                frame.push(func_result)
 
           else:
             raise InvalidType(
@@ -177,7 +189,7 @@ class Interpreter:
           continue
 
         elif op == INST_POP_JMP_IF_FALSE:
-          value = stack.pop()
+          value = frame.pop()
           if value.type != TYPE_BOOLEAN:
             raise InvalidType(u"여기에는 참 또는 거짓 타입을 필요로 합니다.")
 
@@ -187,37 +199,37 @@ class Interpreter:
 
         elif op == INST_FREE_VAR_LOCAL:
           value = frame.load_reserve(inst.operand_int)
-          func = stack.pop().copy()
+          func = frame.pop().copy()
           func.funcval.free_vars.append(value)
-          stack.append(func)
+          frame.push(func)
 
         elif op == INST_FREE_VAR_FREE:
           value = code_object.free_vars[inst.operand_int]
-          func = stack.pop().copy()
+          func = frame.pop().copy()
           func.funcval.free_vars.append(value)
-          stack.append(func)
+          frame.push(func)
 
         elif op == INST_NEGATE:
-          value = stack.pop()
-          stack.append(value.negate())
+          value = frame.pop()
+          frame.push(value.negate())
         else:
-          rhs, lhs = stack.pop(), stack.pop()
+          rhs, lhs = frame.pop(), frame.pop()
           if op == INST_ADD:
-            stack.append(lhs.add(rhs))
+            frame.push(lhs.add(rhs))
           elif op == INST_SUBTRACT:
-            stack.append(lhs.subtract(rhs))
+            frame.push(lhs.subtract(rhs))
           elif op == INST_MULTIPLY:
-            stack.append(lhs.multiply(rhs))
+            frame.push(lhs.multiply(rhs))
           elif op == INST_DIVIDE:
-            stack.append(lhs.divide(rhs))
+            frame.push(lhs.divide(rhs))
           elif op == INST_MOD:
-            stack.append(lhs.mod(rhs))
+            frame.push(lhs.mod(rhs))
           elif op == INST_EQUAL:
-            stack.append(lhs.equal(rhs))
+            frame.push(lhs.equal(rhs))
           elif op == INST_LESS_THAN:
-            stack.append(lhs.less_than(rhs))
+            frame.push(lhs.less_than(rhs))
           elif op == INST_GREATER_THAN:
-            stack.append(lhs.greater_than(rhs))
+            frame.push(lhs.greater_than(rhs))
 
         pc += 1
       except HaneulError as e:
@@ -225,7 +237,7 @@ class Interpreter:
           e.error_line = inst.line_number
         raise e
 
-    if len(stack) == 0:
+    if frame.stack_top == 0:
       return None
     else:
-      return stack.pop()
+      return frame.pop()
